520f86fb7b13458346add7a453174da76b3742a2
tebukuro <58774766+tebukurokun@users.noreply.github.com>
2022-02-10 15:19:30 +0900

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,6 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import reactRefresh from "@vitejs/plugin-react-refresh"
-
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import reactRefresh from "@vitejs/plugin-react-refresh";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -19,10 +18,11 @@ export default defineConfig({
       // port: 443,
     },
   },
+  base: "./",
   define: {
     "process.env": {},
   },
   esbuild: {
     jsxInject: "import React from 'react';",
   },
-})
+});
