7cb6485c0b0e640c459b4807f5080367b77558ef
tebukuro <58774766+tebukurokun@users.noreply.github.com>
2022-02-10 14:05:44 +0900

    improve word filter

--- src/components/WordListArea.tsx
+++ src/components/WordListArea.tsx
@@ -1,37 +1,49 @@
 import { TextArea } from "@blueprintjs/core";
+import { useEffect, useState } from "react";
 import { letterColorSelecters } from "../states";
 import { WordList } from "../util/WordList";
 
 export const WordListArea = () => {
   const letterColorState = letterColorSelecters.useLetterColor();
+  const [wordList, setwordList] = useState(WordList);
 
-  const newWordList = WordList.filter((word) => {
-    return (
-      letterColorState.grayLetters.every(
+  useEffect(() => {
+    const newWordList = WordList.filter((word) => {
+      const trueGrayLetters = letterColorState.grayLetters.filter(
         (s) =>
           !letterColorState.yellowLetters.map((y) => y.letter).includes(s) &&
-          !word.includes(s.toLowerCase())
-      ) &&
-      letterColorState.yellowLetters.every(
-        (s) =>
-          !letterColorState.greenLetters
-            .map((g) => g.letter)
-            .includes(s.letter) && word.includes(s.letter.toLowerCase())
-      ) &&
-      letterColorState.yellowLetters.every(
-        (s) => word[s.index] != s.letter.toLowerCase()
-      ) &&
-      letterColorState.greenLetters.every(
-        (s) => word[s.index] == s.letter.toLowerCase()
-      )
-    );
-  });
+          !letterColorState.greenLetters.map((g) => g.letter).includes(s)
+      );
+      return trueGrayLetters.every((s) => !word.includes(s.toLowerCase()));
+    })
+      .filter((word) => {
+        return letterColorState.yellowLetters.every((s) =>
+          word.includes(s.letter.toLowerCase())
+        );
+      })
+      .filter((word) => {
+        return letterColorState.greenLetters.every(
+          (s) => word[s.index] == s.letter.toLowerCase()
+        );
+      })
+      .filter((word) => {
+        return letterColorState.yellowLetters.every(
+          (s) => word[s.index] != s.letter.toLowerCase()
+        );
+      });
 
-  console.debug(newWordList.length);
+    console.debug(newWordList.length);
+    if (newWordList.length <= 20) {
+      console.debug(newWordList);
+    }
+
+    setwordList(newWordList);
+  }, [letterColorState]);
+
   return (
     <div id="wordListArea" className="">
       <TextArea
-        value={newWordList.join("\n")}
+        value={wordList.join("\n")}
         readOnly
         rows={15}
         fill={true}
